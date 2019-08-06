7e8c412be4daf5b6c96ea3018630552035827304
Stellan Gil <lotstar87@gmail.com>
2019-08-06 12:45:57 +0900

    Initial commit

---
+++ server/middlewares/index.ts
@@ -0,0 +1,23 @@
+const OS = require('os')
+var ip = require('ip')
+var mdns = require('mdns')
+var ad
+
+process.on('bootstrap-module-middleware' as any, app => {
+  /* app에 middleware를 추가할 수 있다. */
+  var servicePort = process.env.PORT
+  var ipAddress = ip.address()
+
+  ad = mdns.createAdvertisement(mdns.tcp('tfserver'), 1008, {
+    name: `Things factory server - ${OS.hostname()}`,
+    txtRecord: {
+      address: `${ipAddress}:${servicePort}`
+    }
+  })
+
+  ad.start()
+})
+
+process.on('exit' as any, code => {
+  if (ad) ad.stop()
+})
